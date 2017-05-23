d3dd63cb96e74eccbce181ce864ba792d09a90e7
sangrammcafee <sangram.m85@gmail.com>
2017-05-23 09:00:42 +0530

    Add files via upload

---
+++ module2_StartsWithEndsWith.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      smohapa2
+#
+# Created:     25-08-2015
+# Copyright:   (c) smohapa2 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+'''
+inputStr = raw_input("Enter ur string:")
+print inputStr
+for ec in inputStr:
+    #print ec
+    print chr(ord(ec)+1),
+'''
+tempString1 = "abc.py"
+if tempString1.endswith('.py1'):
+    print tempString1
+else:
+    print "Not able to search"
+
+
+
+monthName = "April"
+if monthName.startswith('A') and monthName.endswith('l'):
+    print monthName
+
+
+
